37cc9e48f76738c9ccca61165ba99fe5c5eeac1a
p111222 <princejainpre.ram@gmail.com>
2024-11-25 15:20:24 +0530

    Initial commit

--- Client/src/Axios.jsx
+++ Client/src/Axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8080/api';
-// const BASE_URL = 'http://api.kriate.co.in:8344/api';
+// const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = 'http://43.204.108.73:8344/api';
 
 export default axios.create({
     baseURL: BASE_URL
